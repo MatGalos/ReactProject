95c241412d5f4dbc87e2dbc564566eb4100f2201
MatGalos <67073319+MatGalos@users.noreply.github.com>
2021-04-22 17:21:37 +0200

    #9 new elements and starting to work on api import

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -5,8 +5,19 @@ import SidePanel from './Components/common/sidePanel/SidePanel';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MainPanel from './Components/common/MainPanel/MainPanel'
 import Entities from "./Components/mainContent/entitiesPage/Entities";
-//import Workspaces from "./Components/mainContent/workspacePage/Workspaces";
-//import Profile from "./Components/mainContent/profilesPage/Profile";
+import Workspaces from "./Components/mainContent/workspacePage/Workspaces";
+import Profile from "./Components/mainContent/profilesPage/Profile";
+import Administration from './Components/mainContent/administrationPage/administration';
+import ClientContact from './Components/mainContent/clientContactPage/clientContact';
+import Corporate from './Components/mainContent/corporatePage/corporate';
+import Ecosystem from './Components/mainContent/ecosystemPage/ecosystem';
+import Group from './Components/mainContent/groupPage/group';
+import People from './Components/mainContent/peoplePage/people';
+import Privacy from './Components/mainContent/privacyPage/privacy';
+import Publications from './Components/mainContent/publicationsPage/publication';
+import RealEstateContact from './Components/mainContent/realEstateContactPage/realEstateContact';
+import Settings from './Components/mainContent/settingsPage/settings'
+import SupplierContact from './Components/mainContent/supplierContactPage/supplierContact';
 
 const Wrapper = styled.div`
     justify-content: center;
@@ -22,22 +33,61 @@ const Content = styled.div`
 
 const App : FC = () => {
   return (
-    <Router>
-      <Wrapper>
-        <MainHeader/>
-        <Content>
-          <SidePanel/>
-            <Switch>
-              <Route path="/entities">
-                <Entities />
-              </Route>
-              <Route path="/">  
-                <MainPanel />
-              </Route>
-            </Switch>
-        </Content>
-      </Wrapper>
-    </Router>
+      <Router>
+        <Wrapper>
+          <MainHeader/>
+          <Content>
+            <SidePanel/>
+              <Switch>
+                <Route path="/entities">
+                  <Entities />
+                </Route>
+                <Route path="/workspace">  
+                  <Workspaces />
+                </Route>
+                <Route path="/profile">  
+                  <Profile />
+                </Route>
+                <Route path="/administration">  
+                  <Administration />
+                </Route>
+                <Route path="/publications">  
+                  <Publications />
+                </Route>
+                <Route path="/people">  
+                  <People />
+                </Route>
+                <Route path="/client_contact">  
+                  <ClientContact />
+                </Route>
+                <Route path="/supplier_contact">  
+                  <SupplierContact />
+                </Route>
+                <Route path="/corporate">  
+                  <Corporate />
+                </Route>
+                <Route path="/group">  
+                  <Group />
+                </Route>
+                <Route path="/real_estate_contact">  
+                  <RealEstateContact />
+                </Route>
+                <Route path="/privacy">  
+                  <Privacy />
+                </Route>
+                <Route path="/settings">  
+                  <Settings />
+                </Route>
+                <Route path="/ecosystem">  
+                  <Ecosystem />
+                </Route>
+                <Route path="/">  
+                  <MainPanel />
+                </Route>
+              </Switch>
+          </Content>
+        </Wrapper>
+      </Router>
   );
 }
 
